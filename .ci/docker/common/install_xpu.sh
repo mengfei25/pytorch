8858f5a922475dbfabe3dbdf207e6605adc399ef
mengfeil <mengfei.li@intel.com>
2024-03-26 13:12:21 +0800

    LTS driver + oneAPI 2024.1.1

--- .ci/docker/common/install_xpu.sh
+++ .ci/docker/common/install_xpu.sh
@@ -12,8 +12,8 @@ set -xe
 
 function install_ubuntu() {
     # Driver
-    sudo apt update
-    sudo apt install -y gpg-agent wget rsync
+    apt update
+    apt install -y gpg-agent wget rsync
 
     . /etc/os-release
     if [[ ! " jammy " =~ " ${VERSION_CODENAME} " ]]; then
@@ -22,16 +22,16 @@ function install_ubuntu() {
         wget -qO - https://repositories.intel.com/gpu/intel-graphics.key |sudo gpg --dearmor --output /usr/share/keyrings/intel-graphics.gpg
         echo "deb [arch=amd64 signed-by=/usr/share/keyrings/intel-graphics.gpg] https://repositories.intel.com/gpu/ubuntu ${VERSION_CODENAME}/lts/2350 unified" | \
         sudo tee /etc/apt/sources.list.d/intel-gpu-${VERSION_CODENAME}.list
-        sudo apt update
+        apt update
     fi
-    sudo apt install -y linux-headers-$(uname -r) flex bison xpu-smi # intel-fw-gpu intel-i915-dkms
-    sudo apt install -y \
+    apt install -y linux-headers-$(uname -r) flex bison xpu-smi # intel-fw-gpu intel-i915-dkms
+    apt install -y \
         intel-opencl-icd intel-level-zero-gpu level-zero \
         intel-media-va-driver-non-free libmfx1 libmfxgen1 libvpl2 \
         libegl-mesa0 libegl1-mesa libegl1-mesa-dev libgbm1 libgl1-mesa-dev libgl1-mesa-dri \
         libglapi-mesa libgles2-mesa-dev libglx-mesa0 libigdgmm12 libxatracker2 mesa-va-drivers \
         mesa-vdpau-drivers mesa-vulkan-drivers va-driver-all vainfo hwinfo clinfo
-    sudo apt install -y \
+    apt install -y \
         libigc-dev intel-igc-cm libigdfcl-dev libigfxcmrt-dev level-zero-dev
 
     # oneAPI
