0cb1226f61b132c8cfcaa5562b28ffe8ed05408f
mengfeil <mengfei.li@intel.com>
2025-03-06 10:48:18 +0800

    Revert "add the `torch.float8_e8m0fnu` dtype to PyTorch (#147466)"
    This reverts commit 382fbcc1e43ae5d46ec148bdfd8dcfb73da81b77.

--- aten/src/ATen/native/cuda/Indexing.cu
+++ aten/src/ATen/native/cuda/Indexing.cu
@@ -708,13 +708,7 @@ void index_put_with_sort_kernel(Tensor & self, const c10::List<std::optional<Ten
             C10_CUDA_KERNEL_LAUNCH_CHECK();
           }),
           AT_EXPAND(AT_ALL_TYPES_AND_COMPLEX),
-          // AT_EXPAND(AT_FLOAT8_TYPES),
-          // TODO(#113663): clean up accumulation behavior in float8 dtypes, accumulate=True
-          // should not be supported here, then reenable AT_FLOAT8_DTYPES
-          kFloat8_e4m3fn,
-          kFloat8_e5m2,
-          kFloat8_e4m3fnuz,
-          kFloat8_e5m2fnuz,
+          AT_EXPAND(AT_FLOAT8_TYPES),
           kComplexHalf,
           kHalf,
           kBool,
@@ -740,13 +734,7 @@ void index_put_with_sort_kernel(Tensor & self, const c10::List<std::optional<Ten
               C10_CUDA_KERNEL_LAUNCH_CHECK();
             }),
             AT_EXPAND(AT_ALL_TYPES_AND_COMPLEX),
-            // AT_EXPAND(AT_FLOAT8_TYPES),
-            // TODO(#113663): clean up accumulation behavior in float8 dtypes, accumulate=True
-            // should not be supported here, then reenable AT_FLOAT8_DTYPES
-            kFloat8_e4m3fn,
-            kFloat8_e5m2,
-            kFloat8_e4m3fnuz,
-            kFloat8_e5m2fnuz,
+            AT_EXPAND(AT_FLOAT8_TYPES),
             kComplexHalf,
             kHalf,
             kBool,
@@ -770,13 +758,7 @@ void index_put_with_sort_kernel(Tensor & self, const c10::List<std::optional<Ten
                 C10_CUDA_KERNEL_LAUNCH_CHECK();
               }),
               AT_EXPAND(AT_ALL_TYPES_AND_COMPLEX),
-              // AT_EXPAND(AT_FLOAT8_TYPES),
-              // TODO(#113663): clean up accumulation behavior in float8 dtypes, accumulate=True
-              // should not be supported here, then reenable AT_FLOAT8_DTYPES
-              kFloat8_e4m3fn,
-              kFloat8_e5m2,
-              kFloat8_e4m3fnuz,
-              kFloat8_e5m2fnuz,
+              AT_EXPAND(AT_FLOAT8_TYPES),
               kComplexHalf,
               kHalf,
               kBool,
@@ -798,13 +780,7 @@ void index_put_with_sort_kernel(Tensor & self, const c10::List<std::optional<Ten
                 C10_CUDA_KERNEL_LAUNCH_CHECK();
               }),
               AT_EXPAND(AT_ALL_TYPES_AND_COMPLEX),
-              // AT_EXPAND(AT_FLOAT8_TYPES),
-              // TODO(#113663): clean up accumulation behavior in float8 dtypes, accumulate=True
-              // should not be supported here, then reenable AT_FLOAT8_DTYPES
-              kFloat8_e4m3fn,
-              kFloat8_e5m2,
-              kFloat8_e4m3fnuz,
-              kFloat8_e5m2fnuz,
+              AT_EXPAND(AT_FLOAT8_TYPES),
               kComplexHalf,
               kHalf,
               kBool,
@@ -829,13 +805,7 @@ void index_put_with_sort_kernel(Tensor & self, const c10::List<std::optional<Ten
               C10_CUDA_KERNEL_LAUNCH_CHECK();
             }),
             AT_EXPAND(AT_ALL_TYPES_AND_COMPLEX),
-            // AT_EXPAND(AT_FLOAT8_TYPES),
-            // TODO(#113663): clean up accumulation behavior in float8 dtypes, accumulate=True
-            // should not be supported here, then reenable AT_FLOAT8_DTYPES
-            kFloat8_e4m3fn,
-            kFloat8_e5m2,
-            kFloat8_e4m3fnuz,
-            kFloat8_e5m2fnuz,
+            AT_EXPAND(AT_FLOAT8_TYPES),
             kComplexHalf,
             kHalf,
             kBool,
